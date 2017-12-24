7750d2b5d4559f7200ff11d2d185b796e19f3e83
elangovana <aparnaelangovan@gmail.com>
2017-12-24 14:46:16 +1100

    Fix testcases

--- tests/test_NGramFeatureExtraction.py
+++ tests/test_NGramFeatureExtraction.py
@@ -13,23 +13,23 @@ Unit tests for FeatureExtraction class
 class TestNGramFeatureExtraction(unittest.TestCase):
 
     def setUp(self):
-        fileConfig(os.path.join(os.path.dirname( __file__), 'logger.ini'))
-
+        fileConfig(os.path.join(os.path.dirname(__file__), 'logger.ini'))
 
     @data((["This is a ngram", "This is also a ngram"], 2, 4)
         , (["This is apple"], 3, 1)
         , (["Jack is a ngram"], 1, 3)
         , (["This"], 1, 1)
+          # , (["This is apple. This is banna"], 3, 2) # Expect n-grams shouldn't span sentences
           )
     @unpack
-    def test_should_extract_ngrams(self, text_vector, n_gram, expected_no_columns):
+    def test_should_extract_ngrams(self, text_vector, n_gram_size, expected_no_of_ngrams):
         """
         Should extract features
         :type text_vector: The text vector
         """
 
         # Arrange
-        sut = NGramFeatureExtraction(n_gram)
+        sut = NGramFeatureExtraction(n_gram_size)
 
         # Act
         actual = sut.extract(text_vector)
@@ -37,5 +37,5 @@ class TestNGramFeatureExtraction(unittest.TestCase):
         # Assert
         actual_dim = len(actual.shape)
         self.assertEquals(actual_dim, 2, "Expecting a 2 dimensional array, instead found {}".format(actual_dim))
-        self.assertEquals(actual.shape[1], expected_no_columns)
+        self.assertEquals(actual.shape[1], expected_no_of_ngrams)
         self.assertEquals(actual.shape[0], len(text_vector))
