86491126b84bebb17c15fdab8dc531f18471bdb1
elangovana <aparnaelangovan@gmail.com>
2018-01-03 21:42:44 +1100

    Cleanup bioc loader

--- BiocLoader.py
+++ BiocLoader.py
@@ -11,6 +11,8 @@ import os
 import tempfile
 import logging
 import collections
+import cPickle as pickle
+import itertools
 
 
 class BiocLoader:
@@ -20,14 +22,16 @@ class BiocLoader:
         self.model = model or ModelLogisticsRegression()
         self.sentence_extractor = BiocSentences().convert_to_vec
         self.logger = logging.getLogger(__name__)
+        self.genenames_normaliser = BiocAnnotationGenes().get_gene_names_to_normalised_dict
+        self.validate_relation = BiocRelation().is_valid
 
-    def parse(self, filename):
+    def parse(self, filename, output_dir=tempfile.mkdtemp()):
         data_rows = []
         labels = []
         with open(filename, 'r') as fp:
             collection = bioc.load(fp)
             for doc in collection.documents:
-                rows_x, rows_y = self._convert_doc_to_flat(doc)
+                rows_x, rows_y = self.convert_bioc_document_to_rows(doc)
                 data_rows.extend(rows_x)
                 labels.extend(rows_y)
 
@@ -42,7 +46,11 @@ class BiocLoader:
                                           tempfile.mkstemp(prefix="data_formatted_features", suffix=".csv")[1])
         self.save_to_file(logs_features_file, (["uid", "docid", "gene1", "gene2"], n_gram_names, ["label"]),
                           (np.array(data_rows)[:, 0:4], features, labels))
-        self.model.train(features, np.array(labels));
+        trained_model = self.model.train(features, np.array(labels));
+
+        # persist trained model
+        with open(os.path.join(output_dir, tempfile.mkstemp(prefix="trained_model")), 'wb') as fd:
+            pickle.dump(trained_model, fd)
 
     def save_to_file(self, logs_features_file, column_names, columnr_data_to_merge):
         c_names = []
@@ -63,23 +71,24 @@ class BiocLoader:
             data = np.concatenate((data, np.array(columnr_data_to_merge[i]).reshape(len(columnr_data_to_merge[i]), w)),
                                   axis=1)
 
-
         data = np.concatenate((np.array(c_names).reshape(1, len(c_names)), data), axis=0)
 
         self.logger.info("Writing features to log file %s", logs_features_file)
 
         np.savetxt(logs_features_file, data, delimiter='|', fmt="%s")
 
-    def _convert_doc_to_flat(self, doc):
+    def convert_bioc_document_to_rows(self, doc):
         result_x = []
         result_y = []
-        gene_to_norm_gene_dict = BiocAnnotationGenes().get_gene_names_to_normalised_dict(doc)
-        relex = BiocRelation()
+
+        gene_to_norm_gene_dict = self.genenames_normaliser(doc)
         genes = gene_to_norm_gene_dict.values()
-        gene_pairs = self._get_gene_pairs(set(genes))
+        gene_pairs = itertools.combinations(list(set(genes)), 2)
+
         # normalise gene names in sentences
         normalised_sentences = PreprocessorNormaliseGenes().normalise_gene_names_bulk(self.sentence_extractor(doc),
                                                                                       gene_to_norm_gene_dict)
+        # extract fragments
         for gene_pair in gene_pairs:
             # construct unique id
             gene1 = gene_pair[0]
@@ -90,17 +99,6 @@ class BiocLoader:
             combined_fragments = "   \t ".join(fragments)
             uid = "{}#{}#{}".format(doc.id, gene1, gene2)
             result_x.append([uid, doc.id, gene1, gene2, combined_fragments, normalised_freqeuncy])
-            result_y.append(relex.is_valid(doc, gene1, gene2))
+            result_y.append(self.validate_relation(doc, gene1, gene2))
 
         return result_x, result_y
-
-    def _get_gene_pairs(self, normalised_genes):
-        result = []
-
-        genes_list = list(normalised_genes)
-        for i in range(0, len(genes_list)):
-            for j in range(i , len(genes_list)):
-                gene1 = genes_list[i]
-                gene2 = genes_list[j]
-                result.append((gene1, gene2))
-        return result
