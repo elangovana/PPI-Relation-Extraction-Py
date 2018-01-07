0bc37eddc295b5f6b6f30654b0fc380bf123cb99
elangovana <aparnaelangovan@gmail.com>
2018-01-07 17:32:18 +1100

    Refactor move model pipeline from biocloader to pipeline

--- BiocLoader.py
+++ BiocLoader.py
@@ -1,24 +1,17 @@
 import bioc
-import numpy as np
-from sklearn.model_selection import KFold
+
 
 from BiocAnnotationGenes import BiocAnnotationGenes
 from BiocRelation import BiocRelation
 from BiocSentences import BiocSentences
-from ModelLogisticsRegresssion import ModelLogisticsRegression
-from ModelScorer import ModelScorer
-from NGramFeatureExtractor import NGramFeatureExtractor
-from PPIFragmentExtractor import PPIFragementExtractor
-from PostProcessingSelfRelation import PostProcessingSelfRelation
+from Pipeline import Pipeline
 from PreprocessorNormaliseGenes import PreprocessorNormaliseGenes
-import os
+
 import tempfile
 import logging
-import collections
-import cPickle as pickle
 import itertools
 
-I_DOCID = 1
+I_RELATIONS = 1
 I_GENE1 = 2
 I_GENE2 = 3
 I_SENTENCES = 4
@@ -33,14 +26,13 @@ class BiocLoader:
 
     def __init__(self, model=None, logs_dir=tempfile.mkdtemp()):
         self.logs_dir = logs_dir
-        self.model = model or ModelLogisticsRegression()
-        self.sentence_extractor = BiocSentences().convert_to_vec
         self.logger = logging.getLogger(__name__)
+
+        self.sentence_extractor = BiocSentences().convert_to_vec
         self.retrieve_gene_names_dict = BiocAnnotationGenes().get_gene_names_to_normalised_dict
-        self.validate_relation = BiocRelation().is_valid
+        self.retrieve_relations = BiocRelation().get_relations
         self.preprocessor_replace_with_norm_genes = PreprocessorNormaliseGenes().normalise_gene_names_bulk
-        self.preprocessor_ngram_feature_extractor = NGramFeatureExtractor().extract
-        self.preprocessor_fragment_extractor = PPIFragementExtractor().extract
+        self.pipeline = Pipeline()
 
     def parse(self, filename, output_dir=tempfile.mkdtemp()):
         # Read file and do preliminary pre processing to form rows of records
@@ -50,113 +42,11 @@ class BiocLoader:
             for doc in collection.documents:
                 rows_x = self.convert_bioc_document_to_rows(doc)
                 data_rows.extend(rows_x)
+
         # subset
         # data_rows =  data_rows[1:100]
 
-        # Extract fragments
-        for r in data_rows:
-            fragments = self.preprocessor_fragment_extractor(r[I_SENTENCES], r[I_GENE1], r[I_GENE2], r[I_GENESINDOC])
-            count_of_valid_fragments = sum(r[I_GENE1] in f and r[I_GENE2] in f for f in fragments)
-            normalised_frequency = count_of_valid_fragments * 100 / (len(r[I_SENTENCES]))
-            combined_fragments = "   \t ".join(fragments)
-            r.append(normalised_frequency)
-            r.append(combined_fragments)
-            r.append(int(r[I_GENE1] == r[I_GENE2]))
-
-        # Test case by titling the number in favour of thumb print 0 to false, by removing some records
-        #  remove_rec=[('21569203','28379874','2335')
-        #  ,('18570893','801','4651')
-        #  ,('15350535','5058','58480')
-        #  ,('26342861','7474','11197')
-        #  ,('26342861','11197','7476')
-        #  ,('20547845','3146','7099')
-        #  ,('20890284','10870','22914')
-        #  ,('20181956','26986','57690')
-        #  ,('18647389','9185','274')
-        # ]
-        #  data_rows = [r for r in data_rows if (r[I_DOCID], r[I_GENE1], r[I_GENE2]) not in remove_rec]
-
-        # Extract ngram features
-        v_ngram_features, n_gram_names = self.preprocessor_ngram_feature_extractor(np.array(data_rows)[:, I_FRAGMENTS])
-        features = v_ngram_features
-        feature_names = n_gram_names
-
-        # Append features to ngrams
-        # Self Relation
-        new_feature = np.array(data_rows)[:, I_SELFRELATION]
-        features = np.concatenate((features, new_feature.reshape(len(new_feature), 1)), axis=1)
-        feature_names.append("SelfRelation")
-
-        # Append features to metadata (not used by model)
-        # Self Relation
-        metadata = np.array(data_rows)[:, 0:I_SENTENCES]
-        metadata_feature_names = ["uid", "docid", "gene1", "gene2"]
-        new_feature = np.array(data_rows)[:, I_SELFRELATION]
-        metadata = np.concatenate((metadata, new_feature.reshape(len(new_feature), 1)), axis=1)
-        metadata_feature_names.append("SelfRelation")
-
-        # Train model
-        labels = np.array(self.get_labels(data_rows))
-        distinct_labels = np.unique(labels)
-        positive_label = True
-
-        self.logger.info("Training model...")
-        self.logger.info("Total number of features used %i. Feature names:\n%s", len(feature_names),
-                         "\n".join(feature_names))
-        trained_model, holdout_f_score = self.model.train(features, labels, metadata_v=metadata)
-        predicted_on_train = trained_model.predict(features)
-
-        # log formatted features to file
-        logs_features_file = os.path.join(self.logs_dir,
-                                          tempfile.mkstemp(prefix="data_formatted_features", suffix=".csv")[1])
-        feature_thumbprint = np.array([["".join(np.array(r).astype(str))] for r in features])
-        self.logger.info("Writing train features labels and predictions to log file %s", logs_features_file)
-        self.save_to_file(logs_features_file,
-                          (metadata_feature_names, ["labels"], ["Pred"], ["thumbprint"], feature_names),
-                          (metadata, labels, np.array(predicted_on_train), feature_thumbprint, features))
-
-        # post processing
-        model_scorer = ModelScorer(labels=distinct_labels, positive_label=positive_label,
-                                   logs_dir=tempfile.gettempdir())
-        col_self_rel_index = 4
-        post_processor = PostProcessingSelfRelation(col_self_rel_index, value_to_match=True, value_to_set=False)
-        k_fold = KFold(n_splits=3, shuffle=True, random_state=42)
-        for train, test in k_fold.split(features):
-            trained_model.fit(features[train], labels[train])
-            pred_test = trained_model.predict(features[test])
-            post_processed_test = post_processor.process(None, pred_test, metadata[test])
-            f, p, r = model_scorer.get_scores(labels[test], post_processed_test)
-            self.logger.info("Kth hold set f-score, after post processing %s", f)
-
-        # persist trained model
-        pickle_file_name = os.path.join(output_dir, tempfile.mkstemp(prefix="trained_model")[1])
-        self.logger.info("Saving model to %s", pickle_file_name)
-        with open(pickle_file_name, 'wb') as fd:
-            pickle.dump(trained_model, fd)
-
-    def save_to_file(self, logs_features_file, column_names, columnr_data_to_merge):
-        c_names = []
-        for c in column_names:
-            c_names.extend(c)
-
-        data = columnr_data_to_merge[0]
-
-        for i in range(1, len(columnr_data_to_merge)):
-
-            if isinstance(columnr_data_to_merge[i][0], np.ndarray):
-                w = columnr_data_to_merge[i].shape[1]
-
-            elif isinstance(columnr_data_to_merge[i][0], collections.Sequence):
-                w = len(columnr_data_to_merge[i][0])
-            else:
-                w = 1
-
-            data = np.concatenate((data, np.array(columnr_data_to_merge[i]).reshape(len(columnr_data_to_merge[i]), w)),
-                                  axis=1)
-
-        data = np.concatenate((np.array(c_names).reshape(1, len(c_names)), data), axis=0)
-
-        np.savetxt(logs_features_file, data, delimiter='|', fmt="%s")
+        self.pipeline.run_model(data_rows, output_dir)
 
     def convert_bioc_document_to_rows(self, doc):
         result_x = []
@@ -175,12 +65,6 @@ class BiocLoader:
             gene1 = gene_pair[0]
             gene2 = gene_pair[1]
             uid = "{}#{}#{}".format(doc.id, gene1, gene2)
-            result_x.append([uid, doc.id, gene1, gene2, normalised_sentences, genes, doc])
+            result_x.append([uid, doc.id, gene1, gene2, normalised_sentences, genes, self.retrieve_relations(doc)])
 
         return result_x
-
-    def get_labels(self, data_rows):
-        labels = []
-        for r in data_rows:
-            labels.append(self.validate_relation(r[I_BIOCDOC], r[I_GENE1], r[I_GENE2]))
-        return labels
