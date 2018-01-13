bee0ce3717760d130aca732e0321ba919cedc683
elangovana <aparnaelangovan@gmail.com>
2018-01-13 15:46:46 +1100

    Fix tests

--- tests/test_run.py
+++ tests/test_run.py
@@ -20,7 +20,8 @@ class TestRun(TestCase):
     def test_run(self, train_data_file):
         # arrange
         sut = Train.run
+        train_file_abs_path = os.path.join(os.path.dirname(__file__), train_data_file)
 
         #assert
-        sut(train_data_file, output_dir=tempfile.mkdtemp(prefix="Train_test_run_"))
+        sut(train_file_abs_path, output_dir=tempfile.mkdtemp(prefix="Train_test_run_"))
 
